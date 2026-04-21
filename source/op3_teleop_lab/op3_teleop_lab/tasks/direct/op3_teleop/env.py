004f7eb85989c92e3de25ffdc3e44448d43e158b
Jordan <90223683+Jdowdy05@users.noreply.github.com>
2026-04-21 15:41:07 -0400

    Register OP3 contact sensor before cloning

--- source/op3_teleop_lab/op3_teleop_lab/tasks/direct/op3_teleop/env.py
+++ source/op3_teleop_lab/op3_teleop_lab/tasks/direct/op3_teleop/env.py
@@ -178,15 +178,15 @@ class OP3TeleopEnv(DirectRLEnv):
 
     def _setup_scene(self) -> None:
         self.robot = Articulation(self.cfg.robot)
+        self.scene.articulations["robot"] = self.robot
         self.contact_sensor = ContactSensor(self.cfg.contact_sensor)
+        self.scene.sensors["contact_sensor"] = self.contact_sensor
         self.cfg.terrain.num_envs = self.scene.cfg.num_envs
         self.cfg.terrain.env_spacing = self.scene.cfg.env_spacing
         self.terrain = self.cfg.terrain.class_type(self.cfg.terrain)
         self.scene.clone_environments(copy_from_source=False)
         if self.device == "cpu":
             self.scene.filter_collisions(global_prim_paths=[self.cfg.terrain.prim_path])
-        self.scene.articulations["robot"] = self.robot
-        self.scene.sensors["contact_sensor"] = self.contact_sensor
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.8, 0.8, 0.8))
         light_cfg.func("/World/Light", light_cfg)
 
